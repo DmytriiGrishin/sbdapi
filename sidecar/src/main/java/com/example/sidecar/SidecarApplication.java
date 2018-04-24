230e3599c74fe7a420e257dae2807ce11c44166d
DGrishin <DGrishin@croc.ru>
2018-04-24 18:26:17 +0300

    sidecar

--- sidecar/src/main/java/com/example/sidecar/SidecarApplication.java
+++ sidecar/src/main/java/com/example/sidecar/SidecarApplication.java
@@ -2,6 +2,7 @@ package com.example.sidecar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.sidecar.EnableSidecar;
 
 @SpringBootApplication
 @EnableSidecar
