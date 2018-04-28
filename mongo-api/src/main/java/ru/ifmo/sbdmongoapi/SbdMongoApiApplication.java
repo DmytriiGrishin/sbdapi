4e98a8722a341965dc8241bda571c077bfeff9eb
Dmitrii Grishin <dmytriigrishin@gmail.com>
2018-04-28 10:39:58 +0300

    Mongo for replica set

--- mongo-api/src/main/java/ru/ifmo/sbdmongoapi/SbdMongoApiApplication.java
+++ mongo-api/src/main/java/ru/ifmo/sbdmongoapi/SbdMongoApiApplication.java
@@ -19,12 +19,9 @@ public class SbdMongoApiApplication {
         return dataFactory;
     }
 
-    @Value("${spring.data.mongodb.uri}")
-    private String mongoUrl;
-
     public @Bean
     MongoClient mongoClient() {
-        return new MongoClient(mongoUrl.split(":")[1], Integer.parseInt(mongoUrl.split(":")[2]));
+        return new MongoClient();
     }
 
     public @Bean
