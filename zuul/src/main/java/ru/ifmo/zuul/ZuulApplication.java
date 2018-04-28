d3d89e8a7eb495872401062377990aae080329df
Dmitrii Grishin <dmytriigrishin@gmail.com>
2018-04-28 04:51:52 +0300

    zuul experements

--- zuul/src/main/java/ru/ifmo/zuul/ZuulApplication.java
+++ zuul/src/main/java/ru/ifmo/zuul/ZuulApplication.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
