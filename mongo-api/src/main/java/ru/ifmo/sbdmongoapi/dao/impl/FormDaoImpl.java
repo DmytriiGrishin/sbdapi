6ff7c1d001bdeea35ab1d1a885c1c503c4371e67
Dmitrii Grishin <dmytriigrishin@gmail.com>
2018-04-28 12:02:04 +0300

    Mongo for replica set

--- mongo-api/src/main/java/ru/ifmo/sbdmongoapi/dao/impl/FormDaoImpl.java
+++ mongo-api/src/main/java/ru/ifmo/sbdmongoapi/dao/impl/FormDaoImpl.java
@@ -4,11 +4,14 @@ package ru.ifmo.sbdmongoapi.dao.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.ScriptOperations;
+import org.springframework.stereotype.Repository;
 import ru.ifmo.sbdmongoapi.dao.FormDao;
 import org.springframework.data.mongodb.core.ScriptOperations;
 import ru.ifmo.sbdmongoapi.dao.FormDaoCustom;
 import ru.ifmo.sbdmongoapi.model.Form;
 
+
+@Repository
 public class FormDaoImpl  implements FormDaoCustom{
 
     @Autowired
